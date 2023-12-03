6f4ab00b2379a004403fb4979aa46dcae349c3de
Dylan <dylan.imail@gmail.com>
2023-12-02 19:46:25 -0500

    client.go

--- client.go
+++ client.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -37,9 +40,80 @@ type Client struct {
 }
 
 // readPump pumps messages from the websocket connection to the room
-func(c *Client) readPump() {
-    defer func() {
-        c.room.unregister <- c
-        c.conn.Close()
-    }()
+func (c *Client) readPump() {
+	defer func() {
+		c.room.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.room.broadcast <- message
+	}
+}
+
+// writePump pumps messages from the room to the websocket connection
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The room closed the channel
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// serveWs handles websocket requests from the peer
+func serveWs(room *Room, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{room: room, conn: conn, send: make(chan []byte, 256)}
+	client.room.register <- client
+
+	// Allow collection of memory referenced by the caller by doing all work in new goroutines
+	go client.writePump()
+	go client.readPump()
 }
