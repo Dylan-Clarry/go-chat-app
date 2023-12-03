2ae061c5cef92bbb62e4162f338793dcd828dd44
Dylan <dylan.imail@gmail.com>
2023-12-02 22:22:03 -0500

    Bubbletea basic menu setup

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -22,7 +26,7 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
-func main() {
+func main2() {
 	flag.Parse()
 	room := newRoom()
 	go room.run()
@@ -34,8 +38,64 @@ func main() {
 		Addr:              *addr,
 		ReadHeaderTimeout: 3 * time.Second,
 	}
-    err := server.ListenAndServe()
-    if err != nil {
-        log.Fatal("LisenAndServe: ", err)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("LisenAndServe: ", err)
+	}
+}
+
+type model struct {
+	menuitems []string
+	cursor    int
+}
+
+func (m model) Init() tea.Cmd {
+    return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+    case tea.KeyMsg:
+        switch msg.String() {
+        case "ctrl+c", "q":
+            return m, tea.Quit
+        case "up", "k":
+            if m.cursor > 0 {
+                m.cursor--
+            }
+        case "down", "j":
+            if m.cursor < len(m.menuitems) - 1 {
+                m.cursor++
+            }
+        case "enter", " ":
+        }
+    }
+    return m, nil
+}
+
+func (m model) View() string {
+    s := "Go Websocket Chat\n"
+    for i, menuitem := range m.menuitems {
+        cursor := " "
+        if m.cursor == i {
+            cursor = ">"
+        }
+        s += fmt.Sprintf("%s %s\n", cursor, menuitem)
+    }
+    s += "\nPress q to quit.\n"
+    return s
+}
+
+func initialModel() model {
+    return model {
+        menuitems: []string{"Join room", "Create room"},
+    }
+}
+
+func main() {
+    p := tea.NewProgram(initialModel())
+    if _, err := p.Run(); err != nil {
+        fmt.Printf("Error running bubbletea program: %v", err)
+        os.Exit(1)
     }
 }
