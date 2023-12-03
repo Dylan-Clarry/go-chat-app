9c50cda4818b14509c00d1f98df98eee4b047f4f
Dylan <dylan.imail@gmail.com>
2023-12-02 19:24:52 -0500

    room.go

--- room.go
+++ room.go
@@ -1,5 +1,44 @@
 package main
 
 type Room struct {
-    clients map[*Client]bool
+	clients map[*Client]bool
+
+	// inbound messages
+	broadcast chan []byte
+
+	// register/unregister requests
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newRoom() *Room {
+	return &Room{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (r *Room) run() {
+    for {
+        select {
+        case client := <-r.register:
+            r.clients[client] = true
+        case client := <-r.unregister:
+            if _, ok := r.clients[client]; ok {
+                delete(r.clients, client)
+                close(client.send)
+            }
+        case message := <-r.broadcast:
+            for client := range r.clients {
+                select {
+                case client.send <- message:
+                default:
+                    close(client.send)
+                    delete(r.clients, client)
+                }
+            }
+        }
+    }
 }
