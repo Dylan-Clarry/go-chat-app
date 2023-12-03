30c37d4d059c9a024ae7d7cdf736af27f75b96e7
Dylan <dylan.imail@gmail.com>
2023-12-02 19:53:12 -0500

    main.go

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-    "fmt"
+	"flag"
+	"log"
+	"net/http"
+	"time"
 )
 
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method 'Get' not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
 func main() {
-    fmt.Print("It's chatting time")
+	flag.Parse()
+	room := newRoom()
+	go room.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(room, w, r)
+	})
+	server := &http.Server{
+		Addr:              *addr,
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+    err := server.ListenAndServe()
+    if err != nil {
+        log.Fatal("LisenAndServe: ", err)
+    }
 }
